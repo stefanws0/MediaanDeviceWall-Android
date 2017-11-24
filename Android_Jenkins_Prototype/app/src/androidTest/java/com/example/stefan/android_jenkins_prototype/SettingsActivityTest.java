1fd9d9d5274cb3ecb77502d312476ecd7b0402c3
Stefan Willems <stefankessel@hotmail.com>
2017-11-24 12:38:22 +0100

    Initialisation of the Android test project

---
+++ Android_Jenkins_Prototype/app/src/androidTest/java/com/example/stefan/android_jenkins_prototype/SettingsActivityTest.java
@@ -0,0 +1,207 @@
+package com.example.stefan.android_jenkins_prototype;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SettingsActivityTest {
+
+    @Rule
+    public ActivityTestRule<SettingsActivity> mActivityTestRule = new ActivityTestRule<>(SettingsActivity.class);
+
+    @Test
+    public void settingsActivityTest() {
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withClassName(is("android.widget.LinearLayout")))),
+                        0),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        ViewInteraction switch_ = onView(
+                allOf(withId(android.R.id.switch_widget),
+                        childAtPosition(
+                                allOf(withId(android.R.id.widget_frame),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        switch_.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.summary), withText("John Smith"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("John Smith")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(android.R.id.summary), withText("Never"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView2.check(matches(withText("Never")));
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(android.R.id.list_container))),
+                        1),
+                        isDisplayed()));
+        linearLayout2.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(android.R.id.edit), withText("John Smith"),
+                        withParent(withClassName(is("android.widget.LinearLayout")))));
+        editText.perform(scrollTo(), replaceText("Stefan Willems"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button1), withText("OK")));
+        appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction linearLayout3 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(android.R.id.list_container))),
+                        2),
+                        isDisplayed()));
+        linearLayout3.perform(click());
+
+        ViewInteraction appCompatCheckedTextView = onView(
+                allOf(withId(android.R.id.text1), withText("When possible"),
+                        childAtPosition(
+                                allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
+                                        withParent(withClassName(is("android.widget.FrameLayout")))),
+                                1),
+                        isDisplayed()));
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(android.R.id.summary), withText("Stefan Willems"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView3.check(matches(withText("Stefan Willems")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(android.R.id.summary), withText("When possible"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView4.check(matches(withText("When possible")));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Omhoog navigeren"),
+                        withParent(allOf(withId(R.id.action_bar),
+                                withParent(withId(R.id.action_bar_container)))),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction linearLayout4 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withClassName(is("android.widget.LinearLayout")))),
+                        1),
+                        isDisplayed()));
+        linearLayout4.perform(click());
+
+        ViewInteraction textView5 = onView(
+                allOf(withId(android.R.id.summary), withText("Standaardbeltoon (Skyline)"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView5.check(matches(withText("Standaardbeltoon (Skyline)")));
+
+        ViewInteraction linearLayout5 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                childAtPosition(
+                                        withId(android.R.id.list_container),
+                                        0)),
+                        2),
+                        isDisplayed()));
+        linearLayout5.check(matches(isDisplayed()));
+
+        ViewInteraction linearLayout6 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(android.R.id.list_container))),
+                        0),
+                        isDisplayed()));
+        linearLayout6.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Omhoog navigeren"),
+                        withParent(allOf(withId(R.id.action_bar),
+                                withParent(withId(R.id.action_bar_container)))),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
